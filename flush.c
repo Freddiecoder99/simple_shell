677d0d6e00a3df7d9aba7f2f806cf6d3194fdb29
Freddiecoder99 <rickyfredo77@gmail.com>
2023-11-15 08:53:59 -0800

    flush

---
+++ flush.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+        int result;
+        FILE* file;
+
+        //call open function to open the file by write mode
+        file = fopen("flush.txt", "w");
+        //checking for errors while opening
+        if (file == NULL)
+        {
+                perror("can't open the file");
+                return 1;
+        }
+
+        //using fprintf function to write inside the file
+        fprintf(file, "Hello form, ");
+        //making sure that the file is flushed
+        fflush(file);
+
+        //we wrote again
+        fprintf(file, "world C,!\n");
+        //closing the file
+        fclose(file);
+        return 0;
+}
