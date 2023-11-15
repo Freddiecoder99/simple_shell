4b837fe2000fdbd550da14909f9f1f5a234d89d1
Freddiecoder99 <rickyfredo77@gmail.com>
2023-11-15 08:26:25 -0800

    signal

---
+++ signal.c
@@ -0,0 +1,22 @@
+#include <signal.h>
+#include <stdio.h>
+#include <unistd.h>
+
+//creation of handler function
+void signalHandler(int signal)
+{
+        printf("Received signal is: %d\n", signal);
+}
+
+int main()
+{
+        signal(SIGINT, signalHandler);
+
+        printf("Running ... pres ctrl_z to exit\n");
+
+        while(1)
+        {
+                sleep(2);
+        }
+        return 0;
+}
